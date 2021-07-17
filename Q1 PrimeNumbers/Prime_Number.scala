f9484eb900859c51f02e6861745c561de28e869c
sathyaudayangi <82775587+sathyaudayangi@users.noreply.github.com>
2021-07-18 01:21:07 +0530

    Check Whether Prime Number

---
+++ Q1 PrimeNumbers/Prime_Number.scala
@@ -0,0 +1,17 @@
+object Prime_Number extends App{
+
+    def gcd(x:Int, y:Int):Int = y match{
+        case 0 => x
+        case y if(y>x) => gcd(y,x)
+        case _ => gcd(y,x%y)
+    }
+
+    def prime(p:Int, n:Int=2):Boolean = n match{
+        case x if(p==x) => true
+        case x if(gcd(p,x)>1) => false
+        case x => prime(p,x+1)
+    }
+
+    print(prime(7))
+    
+}
